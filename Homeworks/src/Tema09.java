cf5217c15f73fbc6587708a6193dbf50ee9b07f4
MikeTremend24 <mihai.dragota@tremend.com>
2017-12-13 11:34:10 +0200

    Java Tipuri de date si operatori - Siruri - Exercitiul 2

---
+++ Homeworks/src/Tema09.java
@@ -0,0 +1,24 @@
+//// Java Tipuri de date si operatori > > Siruri
+
+// Exercitul 2: un program care va sorta sirul dat si la iesire va da sirul vechi si cel nou
+
+import java.util.Arrays;
+
+public class Tema09 {
+    public static void main(String[] args){
+        int[]array = {1,5,33,563,0,2,23,9,9,11,987,23,934,999,43};
+        System.out.println("This is array:" + Arrays.toString(array));
+        Arrays.sort(array);
+        System.out.println("This is a sorted array:" +Arrays.toString(array));
+    }
+}
+
+/* Raspunsuri CJPR_07 - Core Java Programming:
+1.a
+2.a
+3.a
+4.a
+5.a
+6.a
+7.a
+ */
