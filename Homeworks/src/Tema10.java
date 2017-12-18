2314dfabdeb75b89a0540ff7e5c2ec6c676d32ea
MikeTremend24 <mihai.dragota@tremend.com>
2017-12-18 17:31:49 +0200

    Java Tipuri de date si operatori - Siruri - Exercitiul 1

---
+++ Homeworks/src/Tema10.java
@@ -0,0 +1,16 @@
+//// 3. Controlul fluxului, buclele și ramificarea - 1. Controlul fluxului – If
+
+//Exercitiul 1 : dacă x primește o valoare mai mare decât zero, se intră în blocul de cod și x este egal cu 3.
+
+public class Tema10 {
+
+    public static void main(String[] args) {
+
+        int x = 0;
+        if (x > 0) {
+            x = 3;
+
+        }
+    }
+}
+
