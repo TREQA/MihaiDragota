c4be545af58cd6d73295ee52c0753e9ec34a9b1d
MikeTremend24 <mihai.dragota@tremend.com>
2017-12-18 17:59:38 +0200

    Java Controlul fluxului, buclele si ramificare > IF Exercitul 2

---
+++ Homeworks/src/Tema11.java
@@ -0,0 +1,33 @@
+// Java Controlul fluxului, buclele si ramificare > IF
+
+// Exerciţiul 2 Trebuie modificat codul următor pentru a fi scris corect:
+
+
+public class Tema11 {
+    public static void main(String[] args) {
+
+            int x = 1;
+            int y = 2;
+
+            if (x==y){
+                //Logic here
+            }
+            else if(x==0){
+                //Logic here
+                }
+            else{
+                //Logic here
+            }
+        }
+    }
+
+/*
+Raspunsuri intrebari:
+1. a
+2. a
+3. a
+4. a
+5. a
+6. a
+7. b
+*/
