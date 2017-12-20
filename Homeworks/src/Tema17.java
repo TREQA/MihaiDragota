b2c4c4f050be1d67808c5cec958d37ddf773fc4a
MikeTremend24 <mihai.dragota@tremend.com>
2017-12-20 16:42:30 +0200

    Controlul fluxului, buclele și ramificarea. 4. Buclele while şi do while Exerciţiul 2

---
+++ Homeworks/src/Tema17.java
@@ -0,0 +1,30 @@
+//3. Controlul fluxului, buclele și ramificarea. 4. Buclele while şi do while
+
+//Exerciţiul 2
+// Modificați codul din exerciţiul precedent pentru a se executa folosind bucla do-while.
+//int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//int x = 0;
+
+public class Tema17{
+    public static void main(String[]args){
+        int [] arr = {1,2,3,4,5,6,7,8,9,10};
+
+        int x = 0;
+
+        do {
+            System.out.println(x);
+            x++;
+        }while (x<arr.length);
+    }
+}
+
+/*
+Raspuns intrebari
+1.a
+2.a
+3.a
+4.a
+5.a
+6.a
+7.a
+ */
