0b508523a6be91ab1ec1ff67eccc8f5342682289
MikeTremend24 <mihai.dragota@tremend.com>
2017-12-20 17:20:20 +0200

    Programarea orientată pe obiecte în Java - 1. Clase şi obiecte

---
+++ Homeworks/src/Tema18.java
@@ -0,0 +1,37 @@
+//4. Programarea orientată pe obiecte în Java - 1. Clase şi obiecte
+
+
+//Exercitiul 1
+// trebuie creata clasa Animal care va fi definita ca una abstracta, iar apoi trebuie create doua clase
+// Horse si Dog care vor mosteni prima clasa Animal
+
+public class Tema18{
+
+    abstract class Animal{
+
+    }
+    class Horse extends Animal{
+
+    }
+
+    class Dog extends Animal{
+
+    }
+
+// Exercitiul 2
+// trebuie executata instantierea claselor din exercitiul precedent
+
+    Horse horse = new Horse();
+    Dog dog = new Dog();
+}
+
+/*
+Raspunsuri intrebari:
+1.a
+2.a
+3.a
+4.a
+5.a
+6.a
+7.a
+*/
