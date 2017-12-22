0c445d2220504cfaf68a25609880af064b75ee8f
MikeTremend24 <mihai.dragota@tremend.com>
2017-12-22 17:42:50 +0200

    Programarea orientată pe obiecte în Java - 2. Elementele clasei Exercitiul 2

---
+++ Homeworks/src/Tema20.java
@@ -0,0 +1,62 @@
+//4. Programarea orientată pe obiecte în Java - 2. Elementele clasei
+
+//Exercitiul 2
+
+/*
+Trebuie create 3 clase. Prima este clasa Circle si ea contine proprietatea r(raza).
+A doua clasa este Rectangle si contine proprietatile a si b.
+A treia clasa se numeste Calc si contine metoda statica area care, ca parametru, accepta o instanta a clasei
+(Circle sau Rectangle), iar ca rezultat prezinta aria formei introduse. In functie daca in metoda a intrat instanta clasei Circle sau Rectangle,
+va fi exeutata formula de calculare a ariei pentru Circle sau Rectangle.
+ */
+
+public class Tema20 {
+    public static void main(String[] args) {
+
+        Circle k = new Circle(4);
+        System.out.println(Calculare.area(k));
+        Rectangle r = new Rectangle(2, 3);
+        System.out.println(Calculare.area(r));
+    }
+
+    public static class Circle {
+        public double r;
+
+        public Circle(double r) {
+            this.r = r;
+        }
+    }
+
+    public static class Rectangle {
+        public double a;
+        public double b;
+
+        public Rectangle(double a, double b) {
+            this.a = a;
+            this.b = b;
+        }
+    }
+
+    public static class Calculare {
+        public static double area(Object shape) {
+            if (shape.getClass() == Circle.class) {
+                return Math.pow(((Circle) shape).r, 2) * Math.PI;
+            }
+            if (shape.getClass() == Rectangle.class) {
+                return ((Rectangle) shape).a * ((Rectangle) shape).b;
+            }
+            return 0;
+        }
+    }
+}
+
+/*
+Raspunsuri intrebari:
+1.a
+2.a
+3.a
+4.a
+5.a
+6.a
+7.a
+ */
