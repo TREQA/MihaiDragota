dc0dfc78b1c49c035f8bb1ea6152f5661d8a1bd5
MikeTremend24 <mihai.dragota@tremend.com>
2017-12-27 14:56:31 +0200

    4. Programarea orientată pe obiecte în Java - 4. Clase abstracte şi interfeţe Exercitiul 2

---
+++ Homeworks/src/Tema24.java
@@ -0,0 +1,12 @@
+//4. Programarea orientată pe obiecte în Java - 4. Clase abstracte şi interfeţe
+
+//continuare tema23
+
+public class Tema24{
+    public static void main (String[]args){
+        Square s = new Square();
+        s.setxya(100,100,5);
+        System.out.println(s.area());
+        System.out.println(s.perimetru());
+    }
+}
