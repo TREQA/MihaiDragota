dcdde112deab459a7e0626734aa8ebb4d9dbdb6a
MikeTremend24 <mihai.dragota@tremend.com>
2017-12-28 11:13:52 +0200

    5. Tipurile de date obiectuale - 2. Enumerările Exercitiul 1

---
+++ Homeworks/src/Tema33.java
@@ -0,0 +1,19 @@
+// 5. Tipurile de date obiectuale - 2. Enumerările
+
+public class Tema33 {
+    public static void main (String[] args){
+        Server s = new Server("serverId=25&serverName=my name&serverStatus=Stopped");
+        System.out.print(s);
+    }
+}
+
+/*
+Raspuns intrebari:
+1.a
+2.a
+3.a
+4.a
+5.a
+6.a
+7.c
+ */
