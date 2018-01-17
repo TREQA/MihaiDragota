7339f5a79ea7978856ea49d8ce7a1b40889b7488
MikeTremend24 <mihai.dragota@tremend.com>
2018-01-17 14:06:12 +0200

    7. Gestionarea sistemului de fisiere > 3. Fisiere si Foldere

---
+++ Homeworks/src/Tema44.java
@@ -0,0 +1,64 @@
+// 7. Gestionarea sistemului de fisiere > 3. Fisiere si Foldere
+
+//Exercitiu
+/*Creați un program ce va muta fișierul de pe o cale pe alta (opțiunea cut)
+*/
+
+
+import java.io.*;
+
+public class Tema44 {
+
+    public static void main(String[]args){
+        String path = "";
+        String destination= "";
+
+        try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));)
+        {
+            System.out.println("Enter file path: ");
+            path=bufferedReader.readLine();
+
+            System.out.println("Enter destination folder: ");
+            destination = bufferedReader.readLine();
+        }
+        catch (IOException e)
+        {
+            System.out.println(e);
+        }
+
+        File afile = new File(path);
+        File bfile = new File(destination + "\\" + afile.getName());
+
+        try(FileInputStream inStream = new FileInputStream(afile);
+            FileOutputStream outStream = new FileOutputStream(bfile);) {
+            byte[] buffer = new byte[1024];
+
+            int length;
+
+            while ((length = inStream.read(buffer)) > 0) {
+                outStream.write(buffer, 0, length);
+            }
+
+            System.out.println("File is moved successfuly!");
+            inStream.close();
+            outStream.close();
+            afile.delete();
+        }
+        catch (IOException exc)
+        {
+            System.out.println(exc);
+        }
+
+    }
+}
+
+/*
+Raspunsuri intrebari:
+1.a
+2.a
+3.a
+4.a
+5.a
+6.a
+7.a
+ */
